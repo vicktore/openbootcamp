558d4aff0fc04d2c8b4ec687d71edbdee7da2760
vicktore <victorfabiocastro@gmail.com>
2022-10-03 11:47:35 -0500

    Create clase6ejercicio1.py

---
+++ clase6ejercicio1.py
@@ -0,0 +1,9 @@
+class Carro:
+    
+    def __init__(self, marca, color, placa, modelo):
+        # Propiedades
+        self.numero_llantas = 5
+        self.marca = marca
+        self.color = color
+        self.placa = placa
+        self.modelo = modelo
