094c7132bcc16fcb643d50b20491b99d27eb02da
vicktore <victorfabiocastro@gmail.com>
2022-10-03 11:57:18 -0500

    Create clase6ejercicio2.py

---
+++ clase6ejercicio2.py
@@ -0,0 +1,24 @@
+class Alumno:
+
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def __str__(self):
+        return "Nombre: {self.nombre} Nota: {self.nota}"
+
+
+    def is_aprobado(self, nota):
+        if self.nota >= 6:
+            return "Condicion: APROBADO"
+        else:
+            return "Condicion: DESAPROBADO"
+
+
+nombre = "Jose Carlos Joao"
+nota = 8
+
+alumno =Alumno(nombre, nota)
+
+print(alumno)
+print(alumno.is_aprobado(nota))
