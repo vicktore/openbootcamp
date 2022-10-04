3e39f313412644cace8995db1baca615fec026f0
vicktore <victorfabiocastro@gmail.com>
2022-10-04 08:11:03 -0500

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+from operaciones_math import *
+
+number1, number2 = 10, 5
+
+print(f"Suma: {suma(number1, number2)}")
+print(f"Resta: {resta(number1, number2)}")
+print(f"Multiplicacion: {multiplicacion(number1, number2)}")
+print(f"Division: {division(number1, number2)}")
---
+++ operaciones_math.py
@@ -0,0 +1,13 @@
+##operacion de la culculadora
+
+def suma(number1, number2):
+    return number1 + number2
+
+def resta(number1, number2):
+    return number1 - number2
+
+def multiplicacion(number1, number2):
+    return number1 * number2
+
+def division(number1, number2):
+    return number1 / number2
